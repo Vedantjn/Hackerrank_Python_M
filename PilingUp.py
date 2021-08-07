b6d0e64a15aceafbe98cad09a0748aff2fb83bb3
Vedantjn <76029713+Vedantjn@users.noreply.github.com>
2021-08-07 17:27:22 +0530

    Add files via upload

---
+++ PilingUp.py
@@ -0,0 +1,20 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+from collections import deque
+
+def piling(d):
+    while d:
+        large= None
+        if d[-1]>d[0]:
+            large=d.pop()
+        else:
+            large=d.popleft()
+        
+        if len(d)==0:
+            return "Yes"
+        elif d[-1]>large or d[0]>large:
+            return "No"
+            
+for i in range(int(input())):
+    no_of_cubes=int(input())
+    d=deque(map(int,input().split()))
+    print(piling(d))
